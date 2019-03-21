a302f3dc1547405590c60c6d696a5d04560795c5
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-03-21 16:14:02 +0000

    remove symlinked packages and install stable versions

--- posternoRelease.js
+++ posternoRelease.js
@@ -2,32 +2,35 @@
 
 const chalk = require('chalk');
 const axios = require('axios');
-var fs = require('fs');
+const fs = require('fs');
 const logSymbols = require('log-symbols')
 const async = require('async')
 const ora = require('ora');
 const args = require('args')
-var currentPath = process.cwd();
+const execSync = require('child_process').execSync;
+const currentPath = process.cwd();
 const error = chalk.red.bold;
 const success = chalk.green.bold;
 
 // Parse options sent via terminal.
 args
-  .option('username', 'Optional Github username needed when API rate limits have been hit.', '' )
-  .option('password', 'Optional Github password needed when API rate limits have been hit.', '' )
+	.option('username', 'Optional Github username needed when API rate limits have been hit.', '')
+	.option('password', 'Optional Github password needed when API rate limits have been hit.', '')
 
 const flags = args.parse(process.argv)
 
 // Detect that the posterno-components.json file is found.
-if ( ! fs.existsSync( `${currentPath}/posterno-components.json` ) ) {
+if (!fs.existsSync(`${currentPath}/posterno-components.json`)) {
 	console.log();
-	console.log(error('The posterno-components.json file has not been found. Make sure you are within the Posterno plugin folder.') );
+	console.log(error('The posterno-components.json file has not been found. Make sure you are within the Posterno plugin folder.'));
 	console.log();
 	process.exit(1);
 }
 
-// Query github api to retrieve the latest tag assigned to each component.
+const componentsFile = require(`${currentPath}/posterno-components.json`)
+let availableComponents = []
 
+// Query github api to retrieve the latest tag assigned to each component.
 /**
  * Query github api and retrieve a list of tags.
  * @param {string} component the repository we're querying.
@@ -36,7 +39,7 @@ function checkTag(component) {
 
 	let options = {}
 
-	if ( flags.username && flags.password ) {
+	if (flags.username && flags.password) {
 		options = {
 			auth: {
 				username: flags.username,
@@ -45,49 +48,101 @@ function checkTag(component) {
 		}
 	}
 
-	return axios.get(`https://api.github.com/repos/${component}/tags`, options )
+	return axios.get(`https://api.github.com/repos/${component}/tags`, options)
 		.then((response) => {
 			return response;
 		});
 }
 
-const componentsFile = require(`${currentPath}/posterno-components.json`)
-let availableComponents = []
+/**
+ * Clean up composer of all symlinked packages.
+ */
+function composerCleanUp() {
+
+	let packages = ''
+
+	Object.keys(availableComponents).forEach(key => {
+		packages += availableComponents[key].name + ' '
+	});
+
+	execSync(
+		`composer remove ${packages}`, {
+			stdio: 'inherit'
+		}
+	);
+
+}
+
+function composerInstallStablePackages() {
+
+	let packages = ''
+
+	Object.keys(availableComponents).forEach(key => {
+		packages += availableComponents[key].name + ':' + availableComponents[key].version
+	});
+
+	execSync(
+		`composer require ${packages}`, {
+			stdio: 'inherit'
+		}
+	);
+
+}
 
 console.log();
 
-async.forEach(
-	Object.keys(componentsFile.components),
-	function (key, callback) {
+async.forEachOf(componentsFile.components, (value, key, callback) => {
 
-		let repositoryName = componentsFile.components[key]
+	let repositoryName = value
 
-		checkTag(repositoryName)
-			.then(response => {
-				availableComponents.push({
-					name: repositoryName,
-					version: response.data[0].name
-				})
-				console.log(logSymbols.success, success(`Found component "${repositoryName}" at version ${response.data[0].name}`))
-				console.log();
-				callback();
+	checkTag(repositoryName)
+		.then(response => {
+			availableComponents.push({
+				name: repositoryName,
+				version: response.data[0].name
 			})
-			.catch(errordata => {
-				console.log();
-				if (typeof (errordata.response.data.message) !== 'undefined') {
-					console.log(error(errordata.response.data.message))
-				} else {
-					console.log(error('Something went wrong.'))
-				}
-				console.log();
-				callback();
-				process.exit(1);
-			})
-	},
-	function (err) {
+			console.log(logSymbols.success, success(`Found component "${repositoryName}" at version ${response.data[0].name}`))
+			console.log();
+			callback();
+		})
+		.catch(errordata => {
+			console.log();
+			if ( typeof (errordata.response) !== 'undefined' && typeof (errordata.response.data) !== 'undefined' && typeof (errordata.response.data.message) !== 'undefined') {
+				console.log(error(errordata.response.data.message))
+			} else {
+				console.log(error('Something went wrong.'))
+			}
+			console.log();
+			return callback(errordata)
+		})
 
-	}
-);
+}, err => {
+
+	if (err) console.error(err.message);
+
+	async.nextTick(function() {
+
+		console.log(logSymbols.success, success( 'Latest version of all registered components has been retrieved.' ))
+		console.log()
+
+		console.log(chalk.black.bgYellow( 'Now removing locally symlinked packages...' ));
+		console.log()
+
+		composerCleanUp()
+
+		console.log()
+		console.log(logSymbols.success, success( 'All symlinked packages have been successfully removed.' ))
+
+		console.log()
+		console.log(chalk.black.bgYellow( 'Now installing stable packages...' ));
+		console.log()
+
+		composerInstallStablePackages()
+
+	});
+
+});
+
 // Remove all local symlinked - composer dependencies from the Posterno plugin.
 
 // Re-run composer packages installation in preparation of release with the latest stable version of each component.
